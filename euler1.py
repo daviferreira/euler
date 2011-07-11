7bc1d4cd6d46a0aff8f585acb078c08a0e8ab3ac
Davi Ferreira <contato@daviferreira.com>
2011-07-11 15:18:12 -0300

    English, everyone.

--- euler1.py
+++ euler1.py
@@ -1,8 +1,8 @@
 import math
 
-def multiplos_de_3_ou_5():
+def multiples_of_3_or_5():
   for n in xrange(1000):
     if not n % 3  or not n % 5: 
       yield n
 
-print sum(multiplos_de_3_ou_5())
+print sum(multiples_of_3_or_5())
--- euler2.py
+++ euler2.py
@@ -1,20 +1,20 @@
 import math
 
-def sequencia_fibonacci():
+def fibonacci_sequence():
   x, y = 0, 1
   while True:
     yield x
     x, y = y, x+y
 
-def numero_par(sequencia):
-  for numero in sequencia:
+def even_number(sequence):
+  for numero in sequence:
     if not numero % 2:
       yield numero
 
-def abaixo_de_quatro_milhoes(sequencia):      
-  for numero in sequencia:
+def under_four_million(sequence):      
+  for numero in sequence:
     if numero > 4000000:
       break
     yield numero
 
-print sum(numero_par(abaixo_de_quatro_milhoes(sequencia_fibonacci())))
+print sum(even_number(under_four_million(fibonacci_sequence())))
