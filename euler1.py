bfeae44f266e221709fea84a83dfc00eda9b1955
Davi Ferreira <contato@daviferreira.com>
2011-08-25 09:19:24 -0300

    Removing git references to old files.

--- euler1.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def multiples_of_3_or_5():
-  for n in xrange(1000):
-    if not n % 3  or not n % 5: 
-      yield n
-
-print sum(multiples_of_3_or_5())
--- euler12.py
+++
@@ -1,9 +0,0 @@
-from helpers import *
-
-i = 1 
-divisors = 0
-while divisors <= 500:
-  n = triangle_number(i) 
-  print n
-  divisors = divisors_count(n)
-  i += 1
